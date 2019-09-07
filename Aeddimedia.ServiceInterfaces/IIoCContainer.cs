ba82f9bfb1ac54ea2c4d4db4884135d79c037698
Patrick Decker <patrick.aeddi.decker@googlemail.com>
2019-09-07 21:09:35 +0200

    feat: added solution and ServiceInterfaces project

---
+++ Aeddimedia.ServiceInterfaces/IIoCContainer.cs
@@ -0,0 +1,22 @@
+﻿namespace Aeddimedia.ServiceInterfaces
+{
+    /// <summary>
+    /// Interface for a basic IoC container.
+    /// </summary>
+    public interface IIoCContainer
+    {
+        /// <summary>
+        /// Registers an instance of the given type.
+        /// </summary>
+        /// <typeparam name="InterfaceType">The type of the inerface that will be requested.</typeparam>
+        /// <typeparam name="ImplementedType">The type of the actual implementation that will be returned when the interface is requested.</typeparam>
+        void RegisterInstace<InterfaceType, ImplementedType>();
+
+        /// <summary>
+        /// Gets the registered instance of the given type.
+        /// </summary>
+        /// <typeparam name="T">The type.</typeparam>
+        /// <returns>The instance of the type.</returns>
+        T GetInstance<T>();
+    }
+}
---
+++ Aeddimedia.ServiceInterfaces/INavigationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Aeddimedia.ServiceInterfaces
+{
+    /// <summary>
+    /// An interface that contains methods to enable viewmodel based navigation in an MVVM application.
+    /// </summary>
+    public interface INavigationService
+    {
+        /// <summary>
+        /// Displays the corresponding View to the given ViewModel.
+        /// The View is displayed modally in the region to which the ViewModel is registered to.
+        /// </summary>
+        /// <param name="viewModelType">The type of the ViewModel to navigate to.</param>
+        void DisplayViewModally(Type viewModelType);
+
+        /// <summary>
+        /// Displays the corresponding View to the given ViewModel.
+        /// The View is displayed in the region to which the ViewModel is registered to.
+        /// </summary>
+        /// <param name="viewModelType">The type of the ViewModel to navigate to.</param>
+        void DisplayView(Type viewModelType);
+
+        /// <summary>
+        /// Registers a region with the give name.
+        /// </summary>
+        /// <param name="regionElement">The region.</param>
+        /// <param name="regionName">The name of the region.</param>
+        void RegisterRegion(object regionElement, string regionName);
+
+        /// <summary>
+        /// Registers a ViewModel type to the region with the given name.
+        /// </summary>
+        /// <param name="viewModelType">The type of the ViewModel.</param>
+        /// <param name="regionName">The name of the region.</param>
+        void RegisterViewModelToRegion(Type viewModelType, string regionName);
+    }
+}
