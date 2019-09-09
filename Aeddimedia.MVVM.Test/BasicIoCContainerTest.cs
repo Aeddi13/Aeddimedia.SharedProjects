0addb84704089cb2a366bec3fe23a91b977e5e4f
Patrick Decker <patrick.aeddi.decker@googlemail.com>
2019-09-09 18:42:07 +0200

    feat: add mvvm project and testproject with BasicIoCContainer class

---
+++ Aeddimedia.MVVM.Test/BasicIoCContainerTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aeddimedia.MVVM.Test
+{
+    /// <summary>
+    /// Testclass for the <see cref="BasicIoCContainer"/> class.
+    /// </summary>
+    [TestClass]
+    public class BasicIoCContainerTest
+    {
+        [TestMethod]
+        public void WhenGettingInstanceOfType_TheCorrectInstanceShouldBeReturned()
+        {
+            // Arrange
+            var testObject = new BasicIoCContainer();
+            testObject.RegisterInstace<string, string>();
+
+            // Act
+            var actualResult = testObject.GetInstance<string>();
+
+            // Assert
+            Assert.AreEqual(typeof(string), actualResult.GetType());
+        }
+    }
+}
---
+++ Aeddimedia.MVVM/BasicIoCContainer.cs
@@ -0,0 +1,35 @@
+﻿using Aeddimedia.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Aeddimedia.MVVM
+{
+    /// <summary>
+    /// A basic IoC container.
+    /// </summary>
+    public class BasicIoCContainer : IIoCContainer
+    {
+        private Dictionary<Type, Type> _registeredTypes = new Dictionary<Type, Type>();
+
+        /// <inheritdoc />
+        public T GetInstance<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void RegisterInstace<InterfaceType, ImplementedType>()
+        {
+            Type interfaceType = typeof(InterfaceType);
+            Type implementedType = typeof(ImplementedType);
+
+            if (_registeredTypes.ContainsKey(interfaceType))
+            {
+                Debug.Fail(string.Format("An instance of the type {0} has already been registered.", interfaceType));
+            }
+
+            _registeredTypes.Add(interfaceType, implementedType);
+        }
+    }
+}
